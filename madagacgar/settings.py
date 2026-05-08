23e671cdb44a2d5e4648537865a0eb126b15a335
OMAR <OMAR@comets.com>
2026-05-08 22:24:47 +0900

    fix: modify project settings to open static folder

--- madagacgar/settings.py
+++ madagacgar/settings.py
@@ -135,6 +135,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+print("BASE_DIR", BASE_DIR)
+
+STATICFILES_DIRS = [
+    BASE_DIR/"static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
